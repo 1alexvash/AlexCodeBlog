ed04a9908f98e327b93c69ba09f9cc3e5cf6a6d8
Alexander <1alexvash@gmail.com>
2022-07-08 20:49:59 +0200

    Added RelatedPosts component

---
+++ components/Post/RelatedPosts.tsx
@@ -0,0 +1,198 @@
+const RelatedPosts = () => (
+  <div className="related-posts">
+    <h2>Related posts</h2>
+    <div className="related-posts-block">
+      <a href="" className="image">
+        <img src="images/related-posts-1.jpg" alt="" />
+      </a>
+      <div className="inner">
+        <a href="" className="name">
+          Using SWR React Hooks With Next.js’ Incremental Static Regeneration
+          (ISR)
+        </a>
+        <div className="tags">
+          <a href="">#Firebase</a>
+          <a href="">#React</a>
+          <a href="">#Typescript</a>
+        </div>
+      </div>
+    </div>
+    <div className="related-posts-block">
+      <a href="" className="image">
+        <img src="images/related-posts-2.jpg" alt="" />
+      </a>
+      <div className="inner">
+        <a href="" className="name">
+          Client-Side Routing In Next.js
+        </a>
+        <div className="tags">
+          <a href="">#Firebase</a>
+          <a href="">#React</a>
+        </div>
+      </div>
+    </div>
+    <div className="related-posts-block">
+      <a href="" className="image">
+        <img src="images/related-posts-3.jpg" alt="" />
+      </a>
+      <div className="inner">
+        <a href="" className="name">
+          Using SWR React Hooks With Next.js’ Incremental
+        </a>
+        <div className="tags">
+          <a href="">#Firebase</a>
+        </div>
+      </div>
+    </div>
+    <div className="related-posts-block">
+      <a href="" className="image">
+        <img src="images/related-posts-1.jpg" alt="" />
+      </a>
+      <div className="inner">
+        <a href="" className="name">
+          Using SWR React Hooks With Next.js’ Incremental Static Regeneration
+          (ISR)
+        </a>
+        <div className="tags">
+          <a href="">#Firebase</a>
+          <a href="">#React</a>
+          <a href="">#Typescript</a>
+        </div>
+      </div>
+    </div>
+    <div className="related-posts-block">
+      <a href="" className="image">
+        <img src="images/related-posts-2.jpg" alt="" />
+      </a>
+      <div className="inner">
+        <a href="" className="name">
+          Client-Side Routing In Next.js
+        </a>
+        <div className="tags">
+          <a href="">#Firebase</a>
+          <a href="">#React</a>
+        </div>
+      </div>
+    </div>
+    <div className="related-posts-block">
+      <a href="" className="image">
+        <img src="images/related-posts-3.jpg" alt="" />
+      </a>
+      <div className="inner">
+        <a href="" className="name">
+          Using SWR React Hooks With Next.js’ Incremental
+        </a>
+        <div className="tags">
+          <a href="">#Firebase</a>
+        </div>
+      </div>
+    </div>
+    <div className="related-posts-block">
+      <a href="" className="image">
+        <img src="images/related-posts-1.jpg" alt="" />
+      </a>
+      <div className="inner">
+        <a href="" className="name">
+          Using SWR React Hooks With Next.js’ Incremental Static Regeneration
+          (ISR)
+        </a>
+        <div className="tags">
+          <a href="">#Firebase</a>
+          <a href="">#React</a>
+          <a href="">#Typescript</a>
+        </div>
+      </div>
+    </div>
+    <div className="related-posts-block">
+      <a href="" className="image">
+        <img src="images/related-posts-2.jpg" alt="" />
+      </a>
+      <div className="inner">
+        <a href="" className="name">
+          Client-Side Routing In Next.js
+        </a>
+        <div className="tags">
+          <a href="">#Firebase</a>
+          <a href="">#React</a>
+        </div>
+      </div>
+    </div>
+    <div className="related-posts-block">
+      <a href="" className="image">
+        <img src="images/related-posts-3.jpg" alt="" />
+      </a>
+      <div className="inner">
+        <a href="" className="name">
+          Using SWR React Hooks With Next.js’ Incremental
+        </a>
+        <div className="tags">
+          <a href="">#Firebase</a>
+        </div>
+      </div>
+    </div>
+    <div className="related-posts-block">
+      <a href="" className="image">
+        <img src="images/related-posts-1.jpg" alt="" />
+      </a>
+      <div className="inner">
+        <a href="" className="name">
+          Using SWR React Hooks With Next.js’ Incremental Static Regeneration
+          (ISR)
+        </a>
+        <div className="tags">
+          <a href="">#Firebase</a>
+          <a href="">#React</a>
+          <a href="">#Typescript</a>
+        </div>
+      </div>
+    </div>
+    <div className="related-posts-block">
+      <a href="" className="image">
+        <img src="images/related-posts-2.jpg" alt="" />
+      </a>
+      <div className="inner">
+        <a href="" className="name">
+          Client-Side Routing In Next.js
+        </a>
+        <div className="tags">
+          <a href="">#Firebase</a>
+          <a href="">#React</a>
+        </div>
+      </div>
+    </div>
+    <div className="related-posts-block">
+      <a href="" className="image">
+        <img src="images/related-posts-3.jpg" alt="" />
+      </a>
+      <div className="inner">
+        <a href="" className="name">
+          Using SWR React Hooks With Next.js’ Incremental
+        </a>
+        <div className="tags">
+          <a href="">#Firebase</a>
+        </div>
+      </div>
+    </div>
+    <div className="related-posts-block">
+      <a href="" className="image">
+        <img src="images/related-posts-1.jpg" alt="" />
+      </a>
+      <div className="inner">
+        <a href="" className="name">
+          Using SWR React Hooks With Next.js’ Incremental Static Regeneration
+          (ISR)
+        </a>
+        <div className="tags">
+          <a href="">#Firebase</a>
+          <a href="">#React</a>
+          <a href="">#Typescript</a>
+        </div>
+      </div>
+    </div>
+    <a href="" className="btn">
+      See all posts
+    </a>
+  </div>
+);
+
+export default RelatedPosts;
