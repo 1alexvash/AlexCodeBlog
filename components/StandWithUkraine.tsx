8c8870368562b17b3625cb51095f71eb3a255beb
Alexander <1alexvash@gmail.com>
2022-07-08 19:33:07 +0200

    Added StandWithUkraine component

---
+++ components/StandWithUkraine.tsx
@@ -0,0 +1,18 @@
+const StandWithUkraine = () => (
+  <div className="ukraine-support">
+    <div className="container">
+      <div className="ukraine-flex">
+        <img width="20" height="20" src="images/ua-flag.png" alt="" />
+        <a
+          href="https://war.ukraine.ua/support-ukraine/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <strong>#StandWithUkraine</strong> (Find out how you can help)
+        </a>
+      </div>
+    </div>
+  </div>
+);
+
+export default StandWithUkraine;
