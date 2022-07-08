469d303976b8c58c8b24a733c4cf024eef3ca5e9
Alexander <1alexvash@gmail.com>
2022-07-08 19:39:37 +0200

    Added intro component

---
+++ components/Intro.tsx
@@ -0,0 +1,26 @@
+const Intro = () => (
+  <section className="intro-section">
+    <div className="container">
+      <div className="intro-content">
+        <div className="intro-avatar">
+          <div className="image">
+            <img src="images/author-avatar.jpg" alt="" />
+          </div>
+          <div className="name">Alex</div>
+          <div className="job">Full Stack JavaScript Developer</div>
+        </div>
+        <div className="intro-text">
+          <h1>Hello, my name Alex</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vulputate
+            elementum volutpat a, sit amet sed donec et. Adipiscing egestas
+            molestie commodo sed consectetur egestas. Lorem ipsum dolor sit
+            amet, consectetur adipiscing elit.
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+);
+
+export default Intro;
