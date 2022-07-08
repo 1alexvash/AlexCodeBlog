9fa4c890a8c6fc79b55edac059192a650925d6ba
Alexander <1alexvash@gmail.com>
2022-07-08 20:40:11 +0200

    Added BreadCrumbs component

---
+++ components/Post/BreadCrumbs.tsx
@@ -0,0 +1,16 @@
+const BreadCrumbs = () => (
+  <div className="breadcrumbs-outer">
+    <div className="container">
+      <ul className="breadcrumbs">
+        <li>
+          <a href="">Home</a>
+        </li>
+        <li>
+          <span>How To Build Your Own Comment System Using Firebase</span>
+        </li>
+      </ul>
+    </div>
+  </div>
+);
+
+export default BreadCrumbs;
