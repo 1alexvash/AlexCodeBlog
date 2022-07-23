fadc8ec185b911fb92d3cec34f1df70d29734440
Alexander <1alexvash@gmail.com>
2022-07-23 10:50:56 +0300

    Made the page progress sticky, and dynamic

--- components/Post/PageProgress.tsx
+++ components/Post/PageProgress.tsx
@@ -1,7 +1,36 @@
-const PageProgress = () => (
-  <div className="page-progress">
-    <div className="progress-bar" style={{ width: "10%" }}></div>
-  </div>
-);
+import { useEffect, useState } from "react";
+
+const PageProgress = () => {
+  const [width, setWidth] = useState(0);
+
+  const scrollHeight = () => {
+    const element = document.documentElement,
+      ScrollTop = element.scrollTop || document.body.scrollTop,
+      ScrollHeight = element.scrollHeight || document.body.scrollHeight;
+    const percent = (ScrollTop / (ScrollHeight - element.clientHeight)) * 100;
+    setWidth(percent);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", scrollHeight);
+    return () => window.removeEventListener("scroll", scrollHeight);
+  });
+
+  return (
+    <div
+      className="page-progress"
+      style={{
+        position: "sticky",
+        top: -0.5, // Fix for gap pixel on Chrome with slight zoom
+        transition: "300ms",
+      }}
+    >
+      <div
+        className="progress-bar"
+        style={{ width: width + "%", transition: "0.3s" }}
+      />
+    </div>
+  );
+};
 
 export default PageProgress;
