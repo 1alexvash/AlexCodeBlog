e7a338ba06aff73e4fbbc0250e3cd1a715ada6b9
Alexander <1alexvash@gmail.com>
2022-08-28 20:57:25 +0300

    State for the tags list

--- components/Tags.tsx
+++ components/Tags.tsx
@@ -1,20 +1,43 @@
+import { useState } from "react";
+
 interface Props {
   uniqueTags: string[];
 }
 
-const Tags = ({ uniqueTags }: Props) => (
-  <ul className="filter-tags-list">
-    <li>
-      <a href="" className="active">
+const Tags = ({ uniqueTags }: Props) => {
+  const [selectedTags, setSelectedTags] = useState<string[]>([]);
+
+  const noneTagSelected = selectedTags.length === 0;
+  const allTagsSelected = uniqueTags.length === selectedTags.length;
+
+  return (
+    <ul className="filter-tags-list">
+      <li
+        className={noneTagSelected || allTagsSelected ? "active" : ""}
+        onClick={() => setSelectedTags([])}
+      >
         ALL
-      </a>
-    </li>
-    {uniqueTags.map((uniqueTag) => (
-      <li key={uniqueTag}>
-        <a href="">{uniqueTag}</a>
       </li>
-    ))}
-  </ul>
-);
+      {uniqueTags.map((uniqueTag) => (
+        <li
+          key={uniqueTag}
+          className={selectedTags.includes(uniqueTag) ? "active" : ""}
+          onClick={() => {
+            if (selectedTags.includes(uniqueTag)) {
+              const updatedTags = selectedTags.filter(
+                (tag) => tag !== uniqueTag
+              );
+              setSelectedTags(updatedTags);
+            } else {
+              setSelectedTags([...selectedTags, uniqueTag]);
+            }
+          }}
+        >
+          {uniqueTag}
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 export default Tags;
