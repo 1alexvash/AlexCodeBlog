78a0e7d96ec6ab285e1cd1b8db9ae5600cc68cf3
Alexander <1alexvash@gmail.com>
2022-08-28 10:22:23 +0300

    Fixed serialized JSON error

--- helpers/contentRender.tsx
+++ helpers/contentRender.tsx
@@ -36,16 +36,15 @@ export function getPostBySlug(slug: string, fields: string[] = []) {
     }
   });
 
-  return items;
+  return JSON.parse(JSON.stringify(items));
 }
 
 export function getAllPosts(fields: string[] = []) {
   const slugs = getPostSlugs();
   const posts = slugs
     .map((slug) => getPostBySlug(slug, fields))
-    // sort posts by date in descending order
     .sort((post1, post2) => (post1.date > post2.date ? -1 : 1));
-  return posts;
+  return JSON.parse(JSON.stringify(posts));
 }
 
 export async function markdownToHtml(markdown: string) {
