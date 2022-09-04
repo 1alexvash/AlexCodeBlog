536d3859faee0f3e3f944873b06a0f97d5365393
Alexander <1alexvash@gmail.com>
2022-09-04 09:34:48 +0300

    Support for Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,28 @@ import "../styles/reset.css";
 import "../styles/main.css";
 
 import type { AppProps } from "next/app";
+import Script from "next/script";
 import { Provider } from "react-redux";
 import store from "redux/store";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
+      {/* Google Analytics */}
+      <Script
+        strategy="lazyOnload"
+        src={"https://www.googletagmanager.com/gtag/js?id=G-1JHZSH8YH4"}
+      />
+      <Script strategy="lazyOnload" id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-1JHZSH8YH4');
+        `}
+      </Script>
+
       <Component {...pageProps} />
     </Provider>
   );
