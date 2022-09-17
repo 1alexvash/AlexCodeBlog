b0e3aec2caba78e952e02e311062f9dbd546ef0d
VadymChorny <chorrny228@gmail.com>
2022-09-17 20:20:41 +0300

    fix warning

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -5,43 +5,38 @@ import { useAppDispatch } from "redux/typesHooks";
 
 import { updateTags } from "../redux/slices/selectedTags";
 interface Props {
-	post: Post;
+  post: Post;
 }
 
 const PostCard = ({ post }: Props) => {
-	const dispatch = useAppDispatch();
-	const dispatchTags = (e: any, tag: string) => {
-		e.preventDefault();
-		dispatch(updateTags([tag]))
-	}
-	return (
-		<li>
-			<div className="posts-list-block">
-				<div className="content">
-					<Link href={`/post/${post.slug}`} as={undefined}>
-						<a className="post-img">
-							<Image
-								src={post.featuredImage}
-								layout="fill"
-								alt=""
-								loading="lazy"
-							/>
-						</a>
-					</Link>
-					<div className="tags">
-						{post.tags.map((tag) => (
-							<a href="" key={tag} onClick={(e) => dispatchTags(e, tag)}>
-								#{tag}
-							</a>
-						))}
-					</div>
-					<Link href={`/post/${post.slug}`}>
-						<a className="link">{post.title}</a>
-					</Link>
-				</div>
-			</div>
-		</li>
-	)
+  const dispatch = useAppDispatch();
+  const dispatchTags = (e: any, tag: string) => {
+    e.preventDefault();
+    dispatch(updateTags([tag]));
+  };
+  return (
+    <li>
+      <div className="posts-list-block">
+        <div className="content">
+          <Link href={`/post/${post.slug}`} as={undefined}>
+            <a className="post-img">
+              <Image src={post.featuredImage} layout="fill" alt="" priority />
+            </a>
+          </Link>
+          <div className="tags">
+            {post.tags.map((tag) => (
+              <a href="" key={tag} onClick={(e) => dispatchTags(e, tag)}>
+                #{tag}
+              </a>
+            ))}
+          </div>
+          <Link href={`/post/${post.slug}`}>
+            <a className="link">{post.title}</a>
+          </Link>
+        </div>
+      </div>
+    </li>
+  );
 };
 
 export default PostCard;
