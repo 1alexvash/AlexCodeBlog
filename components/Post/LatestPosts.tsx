574b2ac403a0c8c14ae242b6eeeeb8eeab192dcc
ntorbinskiy <n.torbinskiy@gmail.com>
2022-09-17 11:37:56 +0300

    Tag Optimisation

--- components/Post/LatestPosts.tsx
+++ components/Post/LatestPosts.tsx
@@ -1,39 +1,47 @@
 import Link from "next/link";
 import { Post } from "pages";
+import { useAppDispatch } from "redux/typesHooks";
 
+import { updateTags } from "../../redux/slices/selectedTags";
 interface Props {
-  latestPosts: Post[];
+	latestPosts: Post[];
 }
 
-const LatestPosts = ({ latestPosts }: Props) => (
-  <div className="related-posts">
-    <h2>Latest posts</h2>
-    {latestPosts.map((post) => (
-      <div className="related-posts-block" key={post.title}>
-        <Link href={post.slug}>
-          <a className="image">
-            <img src={post.featuredImage} alt="" />
-          </a>
-        </Link>
-        <div className="inner">
-          <Link href={post.slug}>
-            <a className="name">{post.title}</a>
-          </Link>
-          <div className="tags">
-            {post.tags.map((tag) => (
-              <a href="" key={tag}>
-                #{tag}
-              </a>
-            ))}
-          </div>
-        </div>
-      </div>
-    ))}
+const LatestPosts = ({ latestPosts }: Props) => {
+	const dispatch = useAppDispatch();
 
-    <Link href="/">
-      <a className="btn">See all posts</a>
-    </Link>
-  </div>
-);
+	return (
+		<div className="related-posts">
+			<h2>Latest posts</h2>
+			{latestPosts.map((post) => (
+				<div className="related-posts-block" key={post.title}>
+					<Link href={post.slug}>
+						<a className="image">
+							<img src={post.featuredImage} alt="" />
+						</a>
+					</Link>
+					<div className="inner">
+						<Link href={post.slug}>
+							<a className="name">{post.title}</a>
+						</Link>
+						<div className="tags">
+							{post.tags.map((tag) => (
+								<Link href={"/"} key={tag}>
+									<a href="" key={tag} onClick={() => dispatch(updateTags([tag]))}>
+										#{tag}
+									</a>
+								</Link>
+							))}
+						</div>
+					</div>
+				</div>
+			))}
+
+			<Link href="/">
+				<a className="btn">See all posts</a>
+			</Link>
+		</div>
+	)
+};
 
 export default LatestPosts;
--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -1,38 +1,47 @@
 import Image from "next/image";
 import Link from "next/link";
 import { Post } from "pages";
+import { useAppDispatch } from "redux/typesHooks";
 
+import { updateTags } from "../redux/slices/selectedTags";
 interface Props {
-  post: Post;
+	post: Post;
 }
 
-const PostCard = ({ post }: Props) => (
-  <li>
-    <div className="posts-list-block">
-      <div className="content">
-        <Link href={`/post/${post.slug}`} as={undefined}>
-          <a className="post-img">
-            <Image
-              src={post.featuredImage}
-              layout="fill"
-              alt=""
-              loading="lazy"
-            />
-          </a>
-        </Link>
-        <div className="tags">
-          {post.tags.map((tag) => (
-            <a href="" key={tag}>
-              #{tag}
-            </a>
-          ))}
-        </div>
-        <Link href={`/post/${post.slug}`}>
-          <a className="link">{post.title}</a>
-        </Link>
-      </div>
-    </div>
-  </li>
-);
+const PostCard = ({ post }: Props) => {
+	const dispatch = useAppDispatch();
+	const dispatchTags = (e: any, tag: string) => {
+		e.preventDefault();
+		dispatch(updateTags([tag]))
+	}
+	return (
+		<li>
+			<div className="posts-list-block">
+				<div className="content">
+					<Link href={`/post/${post.slug}`} as={undefined}>
+						<a className="post-img">
+							<Image
+								src={post.featuredImage}
+								layout="fill"
+								alt=""
+								loading="lazy"
+							/>
+						</a>
+					</Link>
+					<div className="tags">
+						{post.tags.map((tag) => (
+							<a href="" key={tag} onClick={(e) => dispatchTags(e, tag)}>
+								#{tag}
+							</a>
+						))}
+					</div>
+					<Link href={`/post/${post.slug}`}>
+						<a className="link">{post.title}</a>
+					</Link>
+				</div>
+			</div>
+		</li>
+	)
+};
 
 export default PostCard;
