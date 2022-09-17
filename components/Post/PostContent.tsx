12554f6152a744649bbf6ae114cefd86ce0f9e6f
Alexander <1alexvash@gmail.com>
2022-09-17 21:33:57 +0300

    Fixing error on the post/slug page

--- components/Post/PostContent.tsx
+++ components/Post/PostContent.tsx
@@ -11,9 +11,9 @@ const PostContent = ({ post }: Props) => (
     <Head>
       <title>{post.title}</title>
       <meta name="description" content={config.site_description} />
-      <meta property="og:title" content={config.page_content[0]} />
+      <meta property="og:title" content={config.site_keywords[0]} />
       <meta property="og:description" content={config.site_description} />
-      <meta property="og:url" content={config.site_url} />
+      <meta property="og:url" content={config.host_url} />
       <meta property="og:type" content="website" />
       <meta property="og:site_name" content={config.site_title} />
       <meta property="og:image" content={config.defaultImage} />
--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -32,7 +32,7 @@ const Post: NextPage<Props> = ({ latestPosts, post }: Props) => (
       <title>{post.title}</title>
       <meta name="description" content={config.site_description} />
       <meta property="og:description" content={config.site_description} />
-      <meta property="og:url" content={config.site_url} />
+      <meta property="og:url" content={config.host_url} />
       <meta property="og:type" content="website" />
       <meta property="og:site_name" content={config.site_title} />
       <meta property="og:image" content={config.defaultImage} />
