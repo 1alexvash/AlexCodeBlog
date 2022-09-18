4a06cf13ae95ac8fb4d3d5d6405ea61dd3668f2d
Alexander <1alexvash@gmail.com>
2022-09-18 12:41:35 +0300

    Providing search feature

--- pages/api/getAllPosts.ts
+++ pages/api/getAllPosts.ts
@@ -1,21 +1,8 @@
+import { getAllPostDocuments } from "helpers/markdownDocumentsReader";
 import type { NextApiRequest, NextApiResponse } from "next";
 
-type Data = {
-  name: string;
-};
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  const posts = getAllPostDocuments();
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  // TODO: fix this
-  // const posts = getAllPosts([
-  //   "slug",
-  //   "title",
-  //   "featuredImage",
-  //   "date",
-  //   "draft",
-  //   "tags",
-  // ]).filter((post: PostDocumentWithoutContent) => post.draft === false);
-  // res.status(200).json(posts);
+  res.status(200).json(posts);
 }
