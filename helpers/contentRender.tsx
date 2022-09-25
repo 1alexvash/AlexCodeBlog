501d529c2554791be5939487caed35a671793eb0
Alexander <1alexvash@gmail.com>
2022-09-25 08:39:31 +0300

    v13

--- helpers/contentRender.tsx
+++ helpers/contentRender.tsx
@@ -15,41 +15,51 @@ export function getPostSlugs() {
   }
 }
 
-export function getPostBySlug(slug: string, fields: string[] = []) {
+function JSONSerialize<Type>(data: Type): Type {
+  return JSON.parse(JSON.stringify(data));
+}
+
+export function getPostBySlug(slug: string) {
   const realSlug = slug.replace(/\.md$/, "");
   const fullPath = join(documentsDirectory, `${realSlug}.md`);
   const fileContents = fs.readFileSync(fullPath, "utf8");
   const { data, content } = matter(fileContents);
 
-  type Items = {
-    [key: string]: string;
-  };
+  return JSONSerialize({ slug: realSlug, ...data, content });
 
-  const items: Items = {};
+  // const realSlug = slug.replace(/\.md$/, "");
+  // const fullPath = join(documentsDirectory, `${realSlug}.md`);
+  // const fileContents = fs.readFileSync(fullPath, "utf8");
+  // const { data, content } = matter(fileContents);
 
-  // Ensure only the minimal needed data is exposed
-  fields.forEach((field) => {
-    if (field === "slug") {
-      items[field] = realSlug;
-    }
-    if (field === "content") {
-      items[field] = content;
-    }
+  // type Items = {
+  //   [key: string]: string;
+  // };
 
-    if (typeof data[field] !== "undefined") {
-      items[field] = data[field];
-    }
-  });
+  // const items: Items = {};
 
-  return JSON.parse(JSON.stringify(items));
+  // // Ensure only the minimal needed data is exposed
+  // fields.forEach((field) => {
+  //   if (field === "slug") {
+  //     items[field] = realSlug;
+  //   }
+  //   if (field === "content") {
+  //     items[field] = content;
+  //   }
+
+  //   if (typeof data[field] !== "undefined") {
+  //     items[field] = data[field];
+  //   }
+  // });
+
+  // return JSON.parse(JSON.stringify(items));
 }
 
 export function getAllPosts(fields: string[] = []) {
   const slugs = getPostSlugs();
 
-  const posts = slugs
-    .map((slug) => getPostBySlug(slug, fields))
-    .sort((post1, post2) => (post1.date > post2.date ? -1 : 1));
+  const posts = slugs.map((slug) => getPostBySlug(slug));
+  // .sort((post1, post2) => (post1.date > post2.date ? -1 : 1));
   return JSON.parse(JSON.stringify(posts));
 }
 
