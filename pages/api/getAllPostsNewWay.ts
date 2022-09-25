103708ca407b970000d65c9cf34042b6507a60dd
Alexander <1alexvash@gmail.com>
2022-09-25 09:42:45 +0300

    Enough to make a PR

--- pages/api/getAllPostsNewWay.ts
+++ pages/api/getAllPostsNewWay.ts
@@ -1,8 +1,10 @@
 import { getAllPostDocuments } from "helpers/markdownDocumentsReader";
 import type { NextApiRequest, NextApiResponse } from "next";
 
+const VERSION = "v1";
+
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const posts = getAllPostDocuments();
 
-  res.status(200).json(posts);
+  res.status(200).json({ posts, VERSION });
 }
