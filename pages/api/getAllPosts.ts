ec22ad2154dc10f0a0be98755653129d203d3455
Alexander <1alexvash@gmail.com>
2022-09-25 09:06:08 +0300

    v13

--- pages/api/getAllPosts.ts
+++ pages/api/getAllPosts.ts
@@ -25,7 +25,7 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
   res.status(200).json({
     posts,
     postSlugs,
-    version: 12,
+    version: 13,
     // aSinglePostExample,
     // documentsDirectory,
   });
