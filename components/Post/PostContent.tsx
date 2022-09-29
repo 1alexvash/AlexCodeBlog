1d48a23d2a8da192c1f748f4dd87d53792c92732
ntorbinskiy <n.torbinskiy@gmail.com>
2022-09-29 12:35:21 +0300

    fixes

--- components/Post/PostContent.tsx
+++ components/Post/PostContent.tsx
@@ -31,7 +31,7 @@ const PostContent = ({ post }: Props) => {
       </Head>
 
       <div className="blogpost-image">
-        <img src={post.featuredImage} alt="blog post image" />
+        <img src={post.featuredImage} alt="blog post image" width={790} height={300} />
       </div>
       <div className="blogpost-date">
         <span>{toHumanReadableDate(post.date)}</span>
