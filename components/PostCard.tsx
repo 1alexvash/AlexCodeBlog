ba27cbe43d53a8214ceef7615fe7b06d26d43d32
Denis Abashin <79473861+fordelord@users.noreply.github.com>
2022-09-29 13:12:20 +0300

    Return fixes

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -17,7 +17,7 @@ const PostCard = ({ post }: Props) => {
         <div className="content">
           <Link href={`/post/${post.slug}`} as={undefined}>
             <a className="post-img" >
-              <img src={post.featuredImage} alt="blog post image" style={{"objectFit": "cover"}}/>
+              <img src={post.featuredImage} alt="blog post image"/>
             </a>
           </Link>
           <div className="tags">
