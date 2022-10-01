195e582c544c1a61d0c72d4eb55de9f7d7a535ef
ntorbinskiy <n.torbinskiy@gmail.com>
2022-10-01 16:20:37 +0300

    code review fixes (second time)

--- config/index.ts
+++ config/index.ts
@@ -1,19 +1,34 @@
 const config = {
-	site_title: "AlexCode",
-	site_description:
-		"Technologies suck, they won't be if you read my blog 💪\nOn my blog I'm sharing tips to increase your productivity and developer happiness",
-	site_keywords: ["Programming post", "Blog about programming"],
-	author_name: "Alex",
-	author_position: "Full Stack JavaScript Developer",
-	social_links: [
-		{ link: "https://t.me/HeWorksSoHard", image: "/images/telegram.svg", width: 20.99, height: 16.99 },
-		{ link: "https://github.com/1alexvash", image: "/images/github-gray.svg", width: 19, height: 19 },
-		{ link: "mailto:1alexvash@gmail.com", image: "/images/mail.svg", width: 24, height: 15.99 },
-	],
-	host_url: "https://alex-code.com/",
-	defaultImage:
-		"https://alex-code.com/post-images/2022-08-05-book-review-so-good-they-cant-ignore-you.webp",
-	posts_per_page: 12,
+  site_title: "AlexCode",
+  site_description:
+    "Technologies suck, they won't be if you read my blog 💪\nOn my blog I'm sharing tips to increase your productivity and developer happiness",
+  site_keywords: ["Programming post", "Blog about programming"],
+  author_name: "Alex",
+  author_position: "Full Stack JavaScript Developer",
+  social_links: [
+    {
+      link: "https://t.me/HeWorksSoHard",
+      image: "/images/telegram.svg",
+      width: 20.99,
+      height: 16.99,
+    },
+    {
+      link: "https://github.com/1alexvash",
+      image: "/images/github-gray.svg",
+      width: 19,
+      height: 19,
+    },
+    {
+      link: "mailto:1alexvash@gmail.com",
+      image: "/images/mail.svg",
+      width: 24,
+      height: 15.99,
+    },
+  ],
+  host_url: "https://alex-code.com/",
+  defaultImage:
+    "https://alex-code.com/post-images/2022-08-05-book-review-so-good-they-cant-ignore-you.webp",
+  posts_per_page: 12,
 };
 
 export default config;
