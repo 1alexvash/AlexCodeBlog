e794f9358911ee23e48f7ea3f5df193330ab6669
ntorbinskiy <n.torbinskiy@gmail.com>
2022-10-05 18:20:07 +0300

    Pulled new files of search

---
+++ components/Header/Search/MobileSkeletons.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+const SkeletonMobile = () => {
+  const titleSkeleton = (
+    <div
+      className="name skeleton"
+      style={{
+        width: "196px",
+        height: "36px",
+        margin: "0 6px 5px",
+      }}
+    />
+  );
+
+  const tagSkeleton = (
+    <div
+      className="skeleton"
+      style={{
+        width: "80px",
+        height: "20px",
+        margin: "0 6px 5px",
+      }}
+    />
+  );
+
+  return (
+    <div className="mobile-posts-block">
+      <div className="inner-flex">
+        <div className="image skeleton" style={{ borderRadius: "3px" }}></div>
+        {titleSkeleton}
+      </div>
+      <div className="tags">
+        {tagSkeleton}
+        {tagSkeleton}
+        {tagSkeleton}
+      </div>
+    </div>
+  );
+};
+
+const MobileSkeletons = () => (
+  <div className="mobile-search-results">
+    {Array.from({ length: 10 }).map((_, index) => (
+      <SkeletonMobile key={index} />
+    ))}
+  </div>
+);
+
+export default MobileSkeletons;
