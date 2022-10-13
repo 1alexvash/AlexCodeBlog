add0e91edfc69d5b5ed1937fde28698f5c283a3a
ntorbinskiy <n.torbinskiy@gmail.com>
2022-10-13 21:51:25 +0300

    Design for future and draft post

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -7,12 +7,48 @@ interface Props {
   post: PostDocumentWithoutContent;
 }
 
+const DraftPost = () => {
+  return (
+    <div className="draft-post">
+      <div className="triangle-draft triangle triangle-item">
+        <span className="triangle-text-draft triangle-text">DRAFT</span>
+      </div>
+      <div className="small-triagles-draft">
+        <div className="small-triagles-item-draft1 triangle small-triagles-item-draft small-triagles-item"></div>
+        <div className="small-triagles-item-draft2 triangle small-triagles-item-draft small-triagles-item"></div>
+      </div>
+    </div>
+  );
+
+  //   draft post flag
+};
+
+const FuturePost = () => {
+  return (
+    <div className="future-post">
+      <div className="triangle-future triangle triangle-item">
+        <span className="triangle-text-future triangle-text">FUTURE</span>
+        <span className="triangle-text-future-post triangle-text"> POST</span>
+      </div>
+      <div className="small-triagles-future">
+        <div className="small-triagles-item-future1 triangle small-triagles-item-future small-triagles-item"></div>
+        <div className="small-triagles-item-future2 triangle small-triagles-item-future small-triagles-item"></div>
+      </div>
+    </div>
+  );
+
+  //   future post flag
+};
+
 const PostCard = ({ post }: Props) => {
   const dispatch = useAppDispatch();
 
   return (
     <li>
-      <div className="posts-list-block">
+      <DraftPost />
+      <FuturePost />
+      <div className="posts-list-block posts-list-block-draft-or-future">
+        {/* posts-list-block-draft-or-future - Class, that gives a PostCard specific styles(turns off hover effect and adds linear gradient) */}
         <div className="content">
           <Link href={`/post/${post.slug}`} as={undefined}>
             <a className="post-img">
