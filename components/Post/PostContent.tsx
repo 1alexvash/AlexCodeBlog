8c4c427ee4aeff95e484c8fa2a9f3f84bfeb2cba
ntorbinskiy <n.torbinskiy@gmail.com>
2022-10-17 18:00:02 +0300

    Added code copy button to the page and copy-function

--- components/Post/PostContent.tsx
+++ components/Post/PostContent.tsx
@@ -2,6 +2,7 @@ import config from "config";
 import toHumanReadableDate from "helpers/toHumanReadableDate";
 import { PostDocument } from "interfaces";
 import Head from "next/head";
+import { useEffect, useRef } from "react";
 
 interface Props {
   post: PostDocument;
@@ -14,8 +15,57 @@ const getFirstParagraph = (str: string) => {
   return str.slice(openedElement, closedElement);
 };
 
+{
+  /* <button class="btn" type="button">
+<div class="btn_squares_div">
+  <div class="btn_square1 btn_squares"></div>
+  <div class="btn_square2 btn_squares"></div>
+</div>
+<span class="btn_text">Copy</span>
+</button>; */
+}
+
+const createCopyButton = (): HTMLButtonElement => {
+  const button = document.createElement("button");
+  const buttonSquaresDiv = document.createElement("div");
+  const buttonSquare1 = document.createElement("div");
+  const buttonSquare2 = document.createElement("div");
+  const buttonText = document.createElement("span");
+
+  button.classList.add("btn_copy");
+  button.setAttribute("type", "button");
+  buttonSquaresDiv.classList.add("btn_copy_squares_div");
+  buttonSquare1.classList.add("btn_copy_square1", "btn_copy_squares");
+  buttonSquare2.classList.add("btn_copy_square2", "btn_copy_squares");
+  buttonText.classList.add("btn_copy_text");
+  buttonText.innerHTML = "Copy";
+
+  button.appendChild(buttonSquaresDiv);
+  buttonSquaresDiv.append(buttonSquare1, buttonSquare2);
+  button.append(buttonText);
+
+  return button;
+};
+
 const PostContent = ({ post }: Props) => {
   const description = getFirstParagraph(post.content);
+  const doc = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const codeBlocks = doc.current?.querySelectorAll("div.remark-highlight");
+
+    if (codeBlocks === undefined) {
+      return;
+    }
+
+    codeBlocks.forEach((item) => {
+      if (item.childNodes.length === 2) {
+        return;
+      }
+
+      item.prepend(createCopyButton());
+    });
+  }, []);
 
   return (
     <article className="blogpost-content">
@@ -50,8 +100,27 @@ const PostContent = ({ post }: Props) => {
           </a>
         ))}
       </div>
+      <div
+        ref={doc}
+        onClick={(e) => {
+          const target = e.target;
 
-      <div dangerouslySetInnerHTML={{ __html: post.content }} />
+          if (
+            target instanceof HTMLButtonElement ||
+            target instanceof HTMLSpanElement
+          ) {
+            navigator.clipboard.writeText(
+              target.parentElement?.childNodes[1].textContent ?? " "
+            );
+          } else if (target instanceof HTMLDivElement) {
+            navigator.clipboard.writeText(
+              target.parentElement?.parentElement?.childNodes[1].textContent ??
+                " "
+            );
+          }
+        }}
+        dangerouslySetInnerHTML={{ __html: post.content }}
+      />
 
       {/* <Reactions /> This future might be added later */}
     </article>
