a065f2c5980a420bbf50eaf3b5ef4f8b57799949
ntorbinskiy <n.torbinskiy@gmail.com>
2022-10-19 13:29:56 +0300

    Added shimer from placeholder example from next.js docs

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -7,6 +7,24 @@ import { setTags } from "../redux/slices/selectedTags";
 interface Props {
   post: PostDocumentWithoutContent;
 }
+const shimmer = (w: number, h: number) => `
+<svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+  <defs>
+    <linearGradient id="g">
+      <stop stop-color="#333" offset="20%" />
+      <stop stop-color="#222" offset="50%" />
+      <stop stop-color="#333" offset="70%" />
+    </linearGradient>
+  </defs>
+  <rect width="${w}" height="${h}" fill="#333" />
+  <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
+  <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
+</svg>`;
+
+const toBase64 = (str: string) =>
+  typeof window === "undefined"
+    ? Buffer.from(str).toString("base64")
+    : window.btoa(str);
 
 const PostCard = ({ post }: Props) => {
   const dispatch = useAppDispatch();
@@ -24,8 +42,11 @@ const PostCard = ({ post }: Props) => {
                 objectFit="cover"
                 priority={true}
                 placeholder="blur"
-                blurDataURL="iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8l8P1HwAGWQJ1HYnHcQAAAABJRU5ErkJggg=="
+                blurDataURL={`data:image/svg+xml;base64,${toBase64(
+                  shimmer(378, 378)
+                )}`}
               />
+              {/* <img src={post.featuredImage} alt="blog post image" /> */}
             </a>
           </Link>
           <div className="tags">
