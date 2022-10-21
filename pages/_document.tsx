c2a1edbbd4deae83553dca929b2c9385658870a6
ntorbinskiy <n.torbinskiy@gmail.com>
2022-10-21 15:57:50 +0300

    Added _document in pages

---
+++ pages/_document.tsx
@@ -0,0 +1,14 @@
+import { Head, Html, Main, NextScript } from "next/document";
+
+const Document = () => {
+  return (
+    <Html>
+      <Head />
+      <body className="dark-theme">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+};
+export default Document;
