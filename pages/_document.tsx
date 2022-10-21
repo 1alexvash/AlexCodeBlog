d44b88dbb8ece017bc2362033cc26100099f2e36
ntorbinskiy <n.torbinskiy@gmail.com>
2022-10-21 17:11:12 +0300

    Added setInitialTheme

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,21 @@
 import { Head, Html, Main, NextScript } from "next/document";
-
 const Document = () => {
+  const setInitialTheme = `function getTheme() {
+	console.log(localStorage.theme);
+	const theme = localStorage.theme;
+	if(theme==='dark') {
+		return 'dark-theme';
+	} else {
+		return 'light-theme';
+	}
+}
+    document.body.classList.add(getTheme())`;
+
   return (
     <Html>
       <Head />
-      <body className="dark-theme">
+      <body>
+        <script dangerouslySetInnerHTML={{ __html: setInitialTheme }} />
         <Main />
         <NextScript />
       </body>
