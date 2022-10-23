81fda30a2617ff0bc6511bfdec3ff6941718d943
ntorbinskiy <n.torbinskiy@gmail.com>
2022-10-23 12:57:55 +0300

    Revert "Fixed bugs"
    This reverts commit c511e561f82716c6187df1a13ac61ffa90371d19.

--- components/DraftImg.tsx
+++ components/DraftImg.tsx
@@ -3,11 +3,15 @@ interface Props {
 }
 
 const DraftImg = ({ height }: Props) => {
+  //height uses to past this Component into different parts of website(LatestPosts,PostContent,PostCard)
+
   return (
     <div className="draft-img" style={{ height: height }}>
       <h1 className="draft-img-text">draft</h1>
     </div>
   );
+
+  //if I haven't got img for draft-post - I'll use this Component
 };
 
 export default DraftImg;
--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -14,12 +14,13 @@ export const DraftPostMark = () => {
       <div className="triangle-draft triangle triangle-item">
         <span className="triangle-text-draft triangle-text">draft</span>
       </div>
-      <div className="small-triangles-draft">
-        <div className="small-triangles-item-draft-first triangle small-triangles-item-draft small-triangles-item"></div>
-        <div className="small-triangles-item-draft-second triangle small-triangles-item-draft small-triangles-item"></div>
+      <div className="small-triagles-draft">
+        <div className="small-triagles-item-draft-first triangle small-triagles-item-draft small-triagles-item"></div>
+        <div className="small-triagles-item-draft-second triangle small-triagles-item-draft small-triagles-item"></div>
       </div>
     </div>
   );
+  //   draft post flag
 };
 
 export const FuturePostMark = () => {
@@ -29,12 +30,14 @@ export const FuturePostMark = () => {
         <span className="triangle-text-future triangle-text">future</span>
         <span className="triangle-text-future-post triangle-text"> post</span>
       </div>
-      <div className="small-triangles-future">
-        <div className="small-triangles-item-future-first triangle small-triangles-item-future small-triangles-item"></div>
-        <div className="small-triangles-item-future-second triangle small-triangles-item-future small-triangles-item"></div>
+      <div className="small-triagles-future">
+        <div className="small-triagles-item-future-first triangle small-triagles-item-future small-triagles-item"></div>
+        <div className="small-triagles-item-future-second triangle small-triagles-item-future small-triagles-item"></div>
       </div>
     </div>
   );
+
+  //   future post flag
 };
 
 const PostCard = ({ post }: Props) => {
@@ -45,6 +48,7 @@ const PostCard = ({ post }: Props) => {
       <DraftPostMark />
       <FuturePostMark />
       <div className="posts-list-block posts-list-block-draft-or-future">
+        {/* posts-list-block-draft-or-future - Class, that gives a PostCard specific styles(turns off hover effect and adds linear gradient) */}
         <div className="content">
           <Link href={`/post/${post.slug}`} as={undefined}>
             <a className="post-img">
@@ -57,6 +61,7 @@ const PostCard = ({ post }: Props) => {
               ) : (
                 <DraftImg height="100%" />
               )}
+              {/* Check have I got an image */}
             </a>
           </Link>
           <div className="tags">
