d6269c2e0ee881f17e88bc3ab5fde41e5b6bb050
NarutoMinecrafter <www.iwanmayor.com@gmail.com>
2022-10-30 02:36:21 +0300

    edits

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,13 @@
+/* eslint-disable @next/next/no-sync-scripts */
 import Document, { Head, Html, Main, NextScript } from "next/document";
-import Script from "next/script";
 
 class CustomDocument extends Document {
   render() {
     return (
       <Html lang="en">
         <Head>
-          <Script src="/scripts/fonts.js" />
-          <Script src="/scripts/theme.js" />
+          <script src="/scripts/fonts.js" />
+          <script src="/scripts/theme.js" />
         </Head>
         <body>
           <Main />
