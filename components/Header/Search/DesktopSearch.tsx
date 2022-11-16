ac0c9b1448419133e96d62c209b096e04380a173
ntorbinskiy <n.torbinskiy@gmail.com>
2022-11-16 13:56:07 +0200

    Edited desktop version of search

--- components/Header/Search/DesktopSearch.tsx
+++ components/Header/Search/DesktopSearch.tsx
@@ -33,79 +33,68 @@ const DesktopSearch = ({
         }));
       }}
     >
-      <div className="desktop-search">
-        <div className="container" onClick={(event) => event.stopPropagation()}>
-          <div className="desktop-search-content">
-            <form
-              action="#"
-              method="post"
-              className="desktop-search-form simple-form"
-            >
-              <div className="input-block">
-                <input
-                  type="text"
-                  placeholder="Site search"
-                  value={search.value}
-                  onChange={(event) => {
-                    setSearch((search) => ({
-                      ...search,
-                      value: event.target.value,
-                    }));
-                  }}
-                  ref={desktopInputRef}
-                />
-              </div>
-            </form>
+      <div
+        className="desktop-search container search-block"
+        onClick={(event) => event.stopPropagation()}
+      >
+        <input
+          type="text"
+          placeholder="Site search"
+          value={search.value}
+          onChange={(event) => {
+            setSearch((search) => ({
+              ...search,
+              value: event.target.value,
+            }));
+          }}
+          ref={desktopInputRef}
+        />
 
-            {search.isLoaded ? (
-              filteredPosts.length > 0 ? (
-                <div className="desktop-search-results">
-                  {filteredPosts.map((post, index) => (
-                    <div className="related-posts-block" key={index}>
-                      <a href={`/post/${post.slug}`} className="image">
-                        <img
-                          src={
-                            post.featuredImage ?? "/post-images/placeholder.png"
-                          }
-                          alt="blog post image"
-                        />
-                      </a>
-                      <div className="inner">
-                        <a href={`/post/${post.slug}`} className="name">
-                          {post.title}
-                        </a>
-                        <div className="tags">
-                          {post.tags.map((tag) => (
-                            <Link href="/" key={tag}>
-                              <a
-                                href=""
-                                key={tag}
-                                onClick={() => {
-                                  setSearch((search) => ({
-                                    ...search,
-                                    showSearch: false,
-                                  }));
+        {search.isLoaded ? (
+          filteredPosts.length > 0 ? (
+            <div className="desktop-search-results">
+              {filteredPosts.map((post, index) => (
+                <div className="related-posts-block" key={index}>
+                  <a href={`/post/${post.slug}`} className="image">
+                    <img
+                      src={post.featuredImage ?? "/post-images/placeholder.png"}
+                      alt="blog post image"
+                    />
+                  </a>
+                  <div className="inner">
+                    <a href={`/post/${post.slug}`} className="name">
+                      {post.title}
+                    </a>
+                    <div className="tags">
+                      {post.tags.map((tag) => (
+                        <Link href="/" key={tag}>
+                          <a
+                            href=""
+                            key={tag}
+                            onClick={() => {
+                              setSearch((search) => ({
+                                ...search,
+                                showSearch: false,
+                              }));
 
-                                  dispatch(setTags([tag]));
-                                }}
-                              >
-                                #{tag}
-                              </a>
-                            </Link>
-                          ))}
-                        </div>
-                      </div>
+                              dispatch(setTags([tag]));
+                            }}
+                          >
+                            #{tag}
+                          </a>
+                        </Link>
+                      ))}
                     </div>
-                  ))}
+                  </div>
                 </div>
-              ) : (
-                <NotFound />
-              )
-            ) : (
-              <DesktopSkeletons />
-            )}
-          </div>
-        </div>
+              ))}
+            </div>
+          ) : (
+            <NotFound />
+          )
+        ) : (
+          <DesktopSkeletons />
+        )}
       </div>
       <div
         className="close-search"
