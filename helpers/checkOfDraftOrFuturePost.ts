2da35de7c6d070bc0d02891a6ae8e85218e0fbff
ntorbinskiy <n.torbinskiy@gmail.com>
2022-11-21 09:29:06 +0200

    Added tomorrow and yesterday for testing
    Fixed hover effect

--- helpers/checkOfDraftOrFuturePost.ts
+++ helpers/checkOfDraftOrFuturePost.ts
@@ -10,6 +10,11 @@ export function isPostADraft(post: PostDocumentWithoutContent) {
   }
 }
 
+const tomorrow = new Date(today);
+tomorrow.setDate(tomorrow.getDate() + 1);
+const yesterday = new Date(today);
+yesterday.setDate(yesterday.getDate() - 1);
+//THIS IS ⬆️ FOR TESTING, DON't MERGE IT
 export function isPostInTheFuture(post: { date: Date | string }) {
   if (today.toISOString() < post.date.toString()) {
     return true;
