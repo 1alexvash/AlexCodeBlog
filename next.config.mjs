469f6e8d6012abcb30a0964f785ef9ed68d30eef
ntorbinskiy <n.torbinskiy@gmail.com>
2022-11-30 22:14:01 +0200

    Added bundle size analyzer

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,17 @@
+import NextBundleAnalyzer from "@next/bundle-analyzer";
 import address from "address";
 import chalk from "chalk";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 };
+
+NextBundleAnalyzer({ enabled: process.env.ANALYZE === "true" });
+
+NextBundleAnalyzer({
+  reactStrictMode: true,
+});
 
 if (process.env.NODE_ENV === "development") {
   console.log(
