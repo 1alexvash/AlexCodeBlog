355ed3c2c9c1298943d312bb07f17266950f6f18
1alexvash <46556479+1alexvash@users.noreply.github.com>
2022-12-17 12:55:25 +0200

    WebSite Improvements (#57)
    * Changed posts per page to 15
    
    As recommended by a designer
    
    * Changed round scroll to the sharp square one
    
    * Clearing clutter page
    
    * Updated blog pictures
    
    * Removed Ubuntu font
    
    To improve website performance, and increased readability
    
    * Updated next to version 13
    
    * Updating to the next 13 link behavior
    
    * Removed redundant node-sass package
    
    * Functional syntax for the Document component
    
    * Running a command to fix next 13 image warnings
    
    https://nextjs.org/docs/messages/next-image-upgrade-to-13
    
    * Merged 2 similar tags into one
    
    * Sorting tags by frequency

--- public/scripts/fonts.js
+++ public/scripts/fonts.js
@@ -9,51 +9,25 @@
     display: "swap",
   };
 
-  // TODO: Needs a potential refactor
-  // const localFonts = [
-  //   {
-  //     source:
-  //       'url(/fonts/Ubuntu-Regular.woff2) format("woff2"), url("/fonts/Ubuntu-Regular.woff") format("woff")',
-  //   },
-  //   {
-  //     source:
-  //       'url(/fonts/Ubuntu-Medium.woff2) format("woff2"), url("/fonts/Ubuntu-Medium.woff") format("woff")',
-  //     descriptors: { weight: "500" },
-  //   },
-  //   {
-  //     source:
-  //       'url(/fonts/Ubuntu-Bold.woff2) format("woff2"), url("/fonts/Ubuntu-Bold.woff") format("woff")',
-  //     descriptors: { weight: "700" },
-  //   },
-  // ];
-  const localFonts = [];
-
   const emojiUnicodes = isApple
     ? []
     : await fetch("/fonts/emojiUnicodes.json").then((response) =>
         response.json()
       );
 
-  const fonts = localFonts
-    .map(
-      ({ family = "Ubuntu", source, descriptors = defaultDescriptors }) =>
-        new FontFace(family, source, {
-          ...descriptors,
-        })
+  const fonts = [].concat(
+    emojiUnicodes.map(
+      (unicode, index) =>
+        new FontFace(
+          "Noto Color Emoji",
+          `url(https://fonts.gstatic.com/s/notocoloremoji/v24/Yq6P-KqIXTD0t4D9z1ESnKM3-HpFabsE4tq3luCC7p-aXxcn.${index}.woff2) format('woff2')`,
+          {
+            ...defaultDescriptors,
+            unicodeRange: unicode,
+          }
+        )
     )
-    .concat(
-      emojiUnicodes.map(
-        (unicode, index) =>
-          new FontFace(
-            "Noto Color Emoji",
-            `url(https://fonts.gstatic.com/s/notocoloremoji/v24/Yq6P-KqIXTD0t4D9z1ESnKM3-HpFabsE4tq3luCC7p-aXxcn.${index}.woff2) format('woff2')`,
-            {
-              ...defaultDescriptors,
-              unicodeRange: unicode,
-            }
-          )
-      )
-    );
+  );
 
   try {
     await Promise.all(
