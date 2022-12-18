183daaf58a2a77746f27fd5c41e34f0580f85c8c
Alexander <1alexvash@gmail.com>
2022-12-18 17:34:46 +0200

    Mini refactor for _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,27 +5,25 @@ import Script from "next/script";
 import { Provider } from "react-redux";
 import store from "redux/store";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <Provider store={store}>
-      {/* Google Analytics */}
-      <Script
-        strategy="lazyOnload"
-        src="https://www.googletagmanager.com/gtag/js?id=G-1JHZSH8YH4"
-      />
-      <Script strategy="lazyOnload" id="google-analytics">
-        {`
+const MyApp = ({ Component, pageProps }: AppProps) => (
+  <Provider store={store}>
+    {/* Google Analytics */}
+    <Script
+      strategy="lazyOnload"
+      src="https://www.googletagmanager.com/gtag/js?id=G-1JHZSH8YH4"
+    />
+    <Script strategy="lazyOnload" id="google-analytics">
+      {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
 
           gtag('config', 'G-1JHZSH8YH4');
         `}
-      </Script>
+    </Script>
 
-      <Component {...pageProps} />
-    </Provider>
-  );
-}
+    <Component {...pageProps} />
+  </Provider>
+);
 
 export default MyApp;
