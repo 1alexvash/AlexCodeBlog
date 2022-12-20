5b6a48fc28eb83e5be20c3c84d0ff261eebcba0c
ntorbinskiy <n.torbinskiy@gmail.com>
2022-12-20 12:28:03 +0200

    Change optimize_id to OPT_CONTAINER_ID

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -20,7 +20,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => (
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
 		
-          gtag('config', 'G-1JHZSH8YH4', { 'optimize_id': 'G-1JHZSH8YH4'});
+          gtag('config', 'G-1JHZSH8YH4', { 'optimize_id': 'OPT_CONTAINER_ID'});
         `}
     </Script>
 
