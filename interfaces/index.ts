ec190299493651354533a42cfcfb756548b5d36b
Alexander <1alexvash@gmail.com>
2023-02-05 17:03:03 +0200

    Mini tweaks

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -2,7 +2,7 @@ export type PostDocument = {
   slug: string;
   title: string;
   date: Date | string;
-  featuredImage?: string | undefined; // TODO: think of something more clever
+  featuredImage?: string | undefined; // TODO: think of something more clever, heroImage might do
   draft: boolean;
   tags: string[];
   content: string;
