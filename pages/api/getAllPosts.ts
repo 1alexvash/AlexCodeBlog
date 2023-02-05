a4d9e1ba87716b772f8d54aca7ab741e734fc04b
Alexander <1alexvash@gmail.com>
2023-02-05 11:08:48 +0200

    Refactor getAllPosts method to not use getAllPostDocuments

--- pages/api/getAllPosts.ts
+++ pages/api/getAllPosts.ts
@@ -1,8 +1,16 @@
-import { getAllPostDocuments } from "helpers/markdownDocumentsReader";
 import type { NextApiRequest, NextApiResponse } from "next";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const posts = getAllPostDocuments();
+import client from ".tina/__generated__/client";
 
-  res.status(200).json(posts);
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const posts = await client.queries.postConnection({});
+
+  // TODO: I should not request the content of the post in here
+  // TODO: check if search is working
+  res
+    .status(200)
+    .json(posts.data.postConnection.edges?.map((edge) => edge?.node).reverse());
 }
