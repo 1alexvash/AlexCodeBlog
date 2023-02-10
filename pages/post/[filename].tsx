0fa6d3b579105926a72e6fdd41bff6b932284260
Alexander <1alexvash@gmail.com>
2023-02-10 10:04:32 +0200

    Beta version of useTina on the blogpost page

--- pages/post/[filename].tsx
+++ pages/post/[filename].tsx
@@ -2,6 +2,7 @@ import config from "config";
 import { PostDocument } from "interfaces";
 import type { NextPage } from "next";
 import Head from "next/head";
+import { useTina } from "tinacms/dist/react";
 
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
@@ -16,33 +17,46 @@ import { client } from "../../.tina/__generated__/client";
 const Post: NextPage<{
   post: any | PostDocument;
   latestPosts: any[]; // The interface is broken by graphql
-}> = ({ post, latestPosts }) => (
-  <>
-    <Head>
-      <title>{post.title}</title>
-      <meta name="description" content={config.site_description} />
-      <meta property="og:description" content={config.site_description} />
-      <meta property="og:url" content={config.host_url} />
-      <meta property="og:type" content="website" />
-      <meta property="og:site_name" content={config.site_title} />
-      <meta property="og:image" content={config.defaultImage} />
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-    <StandWithUkraine />
-    <Header />
-    <BreadCrumbs title={post.title} />
-    <PageProgress />
-    <section className="blogpost-section">
-      <div className="container">
-        <div className="blogpost-outer">
-          <PostContent post={post} />
-          <LatestPosts latestPosts={latestPosts} />
+  data: any;
+  query: any;
+  variables: any;
+}> = (props) => {
+  const { data } = useTina({
+    query: props.query,
+    variables: props.variables,
+    data: props.data,
+  });
+  console.log("data:", data);
+  console.log("props:", props);
+
+  return (
+    <>
+      <Head>
+        {/* <title>{post.title}</title> */}
+        <meta name="description" content={config.site_description} />
+        <meta property="og:description" content={config.site_description} />
+        <meta property="og:url" content={config.host_url} />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content={config.site_title} />
+        <meta property="og:image" content={config.defaultImage} />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <StandWithUkraine />
+      <Header />
+      {/* <BreadCrumbs title={post.title} /> */}
+      <PageProgress />
+      <section className="blogpost-section">
+        <div className="container">
+          <div className="blogpost-outer">
+            {/* <PostContent post={post} /> */}
+            {/* <LatestPosts latestPosts={latestPosts} /> */}
+          </div>
         </div>
-      </div>
-    </section>
-    <Footer />
-  </>
-);
+      </section>
+      <Footer />
+    </>
+  );
+};
 
 type Params = {
   params: {
@@ -52,8 +66,9 @@ type Params = {
 
 export async function getStaticProps({ params }: Params) {
   const relativePath = params.filename + ".md";
+  console.log("relativePath:", relativePath);
 
-  const post = await client.queries.post({ relativePath });
+  const postResponse = await client.queries.post({ relativePath });
 
   const latestPosts = await client.queries.postConnection({
     last: config.latest_posts_per_page,
@@ -61,10 +76,14 @@ export async function getStaticProps({ params }: Params) {
 
   return {
     props: {
-      post: post.data.post,
+      post: postResponse.data.post,
       latestPosts: latestPosts.data.postConnection.edges?.map(
         (edge) => edge?.node
       ),
+
+      data: postResponse.data,
+      query: postResponse.query,
+      variables: postResponse.variables,
     },
   };
 }
