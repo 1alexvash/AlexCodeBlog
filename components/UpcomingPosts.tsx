55f27129a5aa5c08676d551faef3c0f78dbd25d8
Denis Abashin <79473861+fordelord@users.noreply.github.com>
2023-03-01 13:18:46 +0200

    refactor-upcomingPost-component-to-MUI

--- components/UpcomingPosts.tsx
+++ components/UpcomingPosts.tsx
@@ -1,3 +1,4 @@
+import { Box, Divider, Typography } from "@mui/material";
 import { PostDocumentWithoutContent } from "interfaces";
 
 import PostCard from "./PostCard";
@@ -7,17 +8,33 @@ interface Props {
 }
 
 const UpcomingPosts = ({ posts }: Props) => (
-  <div className="upcoming-posts-block">
-    <div className="upcoming-posts-title">
-      <h1>Upcoming Posts</h1>
-    </div>
+  <Box sx={{ mb: 17.5 }}>
+    <Box
+      sx={(theme) => ({
+        color: theme.palette.mode === "light" ? "#3a3a3a" : "#fff",
+      })}
+    >
+      <Typography
+        sx={{
+          lineHeight: 1.28,
+          fontWeight: 700,
+          fontSize: "43px",
+          mb: 9,
+          ["@media (max-width: 480px)"]: {
+            fontSize: "28px",
+          },
+        }}
+      >
+        Upcoming Posts
+      </Typography>
+    </Box>
     <ul className="posts-list">
       {posts.map((post, index) => (
         <PostCard key={index} post={post} />
       ))}
     </ul>
-    <hr />
-  </div>
+    <Divider sx={{ position: "absolute", width: "100%", left: 0 }} />
+  </Box>
 );
 
 export default UpcomingPosts;
