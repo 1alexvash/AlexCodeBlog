b60d90335675387c3cb2258895052097b7bed0f9
Denis Abashin <79473861+fordelord@users.noreply.github.com>
2023-03-03 13:21:41 +0200

    clode-tag-miss-after-merge

--- components/AudioPlayer.tsx
+++ components/AudioPlayer.tsx
@@ -136,7 +136,7 @@ const AudioPlayer = () => {
         src="https://cdn.simplecast.com/audio/cae8b0eb-d9a9-480d-a652-0defcbe047f4/episodes/af52a99b-88c0-4638-b120-d46e142d06d3/audio/500344fb-2e2b-48af-be86-af6ac341a6da/default_tc.mp3"
         preload="metadata"
         onLoadedMetadata={onLoadedMetadata}
-      ></audio>
+      />
       <button onClick={togglePlayPause} className="play">
         {isPlaying ? (
           <img
