9a6ad34dd2dd62fa594703fb524d0b30a9598766
Denis Abashin <79473861+fordelord@users.noreply.github.com>
2023-03-24 21:27:43 +0200

    return meta tegs in slug component

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -28,12 +28,16 @@ const Post: NextPage<{
     <>
       <Head>
         <title>{post.title}</title>
+        <meta property="og:title" content={post.title} />
         <meta name="description" content={config.site_description} />
-        <meta property="og:description" content={config.site_description} />
+        <meta
+          property="og:description"
+          content={getFirstParagraph(post.content)}
+        />
         <meta property="og:url" content={config.host_url} />
         <meta property="og:type" content="website" />
         <meta property="og:site_name" content={config.site_title} />
-        <meta property="og:image" content={config.defaultImage} />
+        <meta property="og:image" content={post.featuredImage} />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <StandWithUkraine />
