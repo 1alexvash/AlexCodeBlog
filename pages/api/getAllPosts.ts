34d9d50438d34a585e9230fd806d1aa78c75eda7
ntorbinskiy <n.torbinskiy@gmail.com>
2023-04-04 19:20:30 +0300

    Remove TODO comment

--- pages/api/getAllPosts.ts
+++ pages/api/getAllPosts.ts
@@ -8,8 +8,6 @@ export default async function handler(
 ) {
   const posts = await client.queries.postConnection({});
 
-  // TODO: I should not request the content of the post in here
-  // TODO: check if search is working
   res
     .status(200)
     .json(posts.data.postConnection.edges?.map((edge) => edge?.node).reverse());
