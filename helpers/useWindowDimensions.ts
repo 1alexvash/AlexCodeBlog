3b6673f194cd10967f3fa3264c489ae9e44d51ea
Denis Abashin <79473861+fordelord@users.noreply.github.com>
2023-04-13 12:00:04 +0300

    add dependence width of window

--- helpers/useWindowDimensions.ts
+++ helpers/useWindowDimensions.ts
@@ -1,12 +1,17 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function useWindowDimensions(callback: Function) {
+  const [width, setWidth] = useState<number>(0);
+
   useEffect(() => {
     function handleResize() {
       callback();
+      setWidth(window.innerWidth);
     }
 
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
-  }, [callback]);
+  }, [callback, width]);
+
+  return { width };
 }
