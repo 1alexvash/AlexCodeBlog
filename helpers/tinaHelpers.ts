d0da8decaa57f2d858b3b72804032d4222ca4edf
Alexander <1alexvash@gmail.com>
2023-04-21 10:46:04 +0300

    Collocate tina helpers

--- helpers/tinaHelpers.ts
+++ helpers/tinaHelpers.ts
@@ -1,7 +1,21 @@
 import { PostDocument, PostFromQuery } from "interfaces";
 
-import postToDocument from "./postToDocument";
 import { PostQuery } from ".tina/__generated__/types";
+
+const postToDocument = (post: PostFromQuery): PostDocument => {
+  const { date, draft, title, body, heroImage, tags, _sys, id } = post;
+
+  return {
+    date,
+    draft,
+    title,
+    body,
+    heroImage: heroImage ?? "",
+    tags: tags ? (tags.filter((tag) => tag !== null) as string[]) : [],
+    _sys,
+    id,
+  };
+};
 
 export const queryToDocument = (data: PostQuery): PostDocument => {
   const { post } = data;
