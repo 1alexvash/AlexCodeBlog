06d6438f6235b045680863097d16cfe0fb77cd47
Alexander <1alexvash@gmail.com>
2023-04-22 13:31:32 +0300

    Widen 'yarn lint' command scope

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,17 @@ const isDev = process.env.NODE_ENV === "development";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    dirs: [
+      "pages",
+      "helpers",
+      "components",
+      "state",
+      "redux",
+      "helpers",
+      "interfaces",
+    ],
+  },
   reactStrictMode: true,
   swcMinify: true,
   rewrites: () => [
