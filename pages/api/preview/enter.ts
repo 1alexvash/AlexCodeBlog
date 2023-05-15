cd3ff02d442239f54ea7037469e0acf751d9e406
ntorbinskiy <n.torbinskiy@gmail.com>
2023-05-15 11:40:30 +0300

    Add tina variant for test

--- pages/api/preview/enter.ts
+++ pages/api/preview/enter.ts
@@ -1,13 +1,31 @@
+import { isUserAuthorized } from "@tinacms/auth";
 import { NextApiRequest, NextApiResponse } from "next";
 
-const handler = async (req: NextApiRequest, res: NextApiResponse<void>) => {
-  const protocol =
-    process.env.NODE_ENV === "development" ? "http://" : "https://";
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (typeof req.query.slug !== "string") {
+    return res.status(400).json({ message: "Invalid slug" });
+  }
+  console.log("handler enter");
+  if (process.env.NODE_ENV === "development") {
+    // Enter preview-mode in local development
+    console.log("development mode - entering preview mode");
+    res.setPreviewData({});
+    return res.redirect(req.query.slug);
+  }
 
-  const baseurl = `${protocol}${req.headers.host ?? ""}/admin`;
+  // Check tina cloud token
+  const isAuthorizedRes = await isUserAuthorized({
+    token: `Bearer ${req.query.token}`,
+    clientID: process.env.NEXT_PUBLIC_TINA_CLIENT_ID ?? "",
+  });
 
-  res.setDraftMode({ enable: true });
-  res.redirect(baseurl);
+  if (isAuthorizedRes) {
+    console.log("production mode - entering preview mode");
+    res.setPreviewData({});
+    return res.redirect(req.query.slug);
+  }
+
+  return res.status(401).json({ message: "Invalid token" });
 };
 
 export default handler;
