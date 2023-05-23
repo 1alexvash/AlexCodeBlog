e8bdd6fccf72f1be8ddffced2fa08aee23b4b49d
ntorbinskiy <n.torbinskiy@gmail.com>
2023-05-23 19:42:51 +0300

    remove middleware for test

--- middleware.ts
+++ middleware.ts
@@ -1,40 +1,40 @@
-import type { NextRequest } from "next/server";
-import { NextResponse } from "next/server";
+// import type { NextRequest } from "next/server";
+// import { NextResponse } from "next/server";
 
-let previousValue: string;
-let initializedPage: { [key: string]: boolean } = {
-  admin: false,
-  client: false,
-};
+// let previousValue: string;
+// let initializedPage: { [key: string]: boolean } = {
+//   admin: false,
+//   client: false,
+// };
 
-export function middleware(request: NextRequest) {
-  const url = request.nextUrl.pathname;
+// export function middleware(request: NextRequest) {
+//   const url = request.nextUrl.pathname;
 
-  if (url === "/admin") {
-    if (!initializedPage.admin) {
-      initializedPage.admin = true;
-      return NextResponse.redirect(
-        new URL("/api/preview/enter?slug=/admin", request.url)
-      );
-    }
-    previousValue = "admin";
-    initializedPage.client = false;
-    return NextResponse.next();
-  }
+//   if (url === "/admin") {
+//     if (!initializedPage.admin) {
+//       initializedPage.admin = true;
+//       return NextResponse.redirect(
+//         new URL("/api/preview/enter?slug=/admin", request.url)
+//       );
+//     }
+//     previousValue = "admin";
+//     initializedPage.client = false;
+//     return NextResponse.next();
+//   }
 
-  if (url === "/") {
-    if (!initializedPage.client && previousValue === "client") {
-      initializedPage.client = true;
-      return NextResponse.redirect(
-        new URL("/api/preview/exit?slug=/", request.url)
-      );
-    }
-    previousValue = "client";
-    initializedPage.admin = false;
-    return NextResponse.next();
-  }
-}
+//   if (url === "/") {
+//     if (!initializedPage.client && previousValue === "client") {
+//       initializedPage.client = true;
+//       return NextResponse.redirect(
+//         new URL("/api/preview/exit?slug=/", request.url)
+//       );
+//     }
+//     previousValue = "client";
+//     initializedPage.admin = false;
+//     return NextResponse.next();
+//   }
+// }
 
-export const config = {
-  matcher: ["/admin", "/"],
-};
+// export const config = {
+//   matcher: ["/admin", "/"],
+// };
