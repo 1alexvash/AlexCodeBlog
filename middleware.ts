6274fb94bab6a0a0484fb46ac6b991c29445bd19
ntorbinskiy <n.torbinskiy@gmail.com>
2023-05-25 16:36:48 +0300

    Revert "Remove middleware for test"
    This reverts commit fdb2a7409a67c94ba54cddda721a43f24573d4a8.

---
+++ middleware.ts
@@ -0,0 +1,36 @@
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+
+let previousValue: string;
+let initializedPage: { [key: string]: boolean } = {
+  admin: false,
+  client: false,
+};
+
+export function middleware(request: NextRequest) {
+  const url = request.nextUrl.pathname;
+
+  if (url === "/admin") {
+    if (!initializedPage.admin) {
+      initializedPage.admin = true;
+      return NextResponse.redirect(new URL("/api/preview/enter", request.url));
+    }
+    previousValue = "admin";
+    initializedPage.client = false;
+    return NextResponse.next();
+  }
+
+  if (url === "/") {
+    if (!initializedPage.client && previousValue === "client") {
+      initializedPage.client = true;
+      return NextResponse.redirect(new URL("/api/preview/exit", request.url));
+    }
+    previousValue = "client";
+    initializedPage.admin = false;
+    return NextResponse.next();
+  }
+}
+
+export const config = {
+  matcher: ["/admin", "/"],
+};
