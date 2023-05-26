33b8fa57b732e8d8289f10ae02e0c4b1e85820e4
ntorbinskiy <n.torbinskiy@gmail.com>
2023-05-26 10:27:57 +0300

    Add /admin/

--- pages/api/preview/enter.ts
+++ pages/api/preview/enter.ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   res.setDraftMode({ enable: true });
-  return res.redirect("/admin");
+  return res.redirect("/admin/");
 };
 
 export default handler;
