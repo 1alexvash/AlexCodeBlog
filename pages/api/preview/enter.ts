eed395c55c29e22c1995c42b669f01589273125c
ntorbinskiy <n.torbinskiy@gmail.com>
2023-05-30 11:19:04 +0300

    Update  route

--- pages/api/preview/enter.ts
+++ pages/api/preview/enter.ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   res.setDraftMode({ enable: true });
-  res.redirect(307, "/admin"); // Use status code 307 for the redirect
+  res.redirect(307, "/admin/index.html"); // Use status code 307 for the redirect
 };
 
 export default handler;
