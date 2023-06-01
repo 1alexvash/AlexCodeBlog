e3c301a2fdb412917905f0f03ac1bb8768a912d9
Denis Abashin <79473861+fordelord@users.noreply.github.com>
2023-06-01 16:14:10 +0300

    add the solution to try on the production mode

--- middleware.ts
+++ middleware.ts
@@ -1,26 +1,34 @@
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
-let previousValue: string;
-let initializedPage: { [key: string]: boolean } = {
+type PreviousValueType = "admin" | "client";
+type InitializedPageType = { [key: string]: boolean };
+
+let previousValue: PreviousValueType = "admin";
+let initializedPage: InitializedPageType = {
   admin: false,
   client: false,
 };
+let isAdmin = false;
 
 export function middleware(request: NextRequest) {
-  const url = request.nextUrl.pathname;
+  const url = request.url;
 
-  if (url === "/admin") {
+  if (url.includes("/admin")) {
+    isAdmin = true;
+
     if (!initializedPage.admin) {
       initializedPage.admin = true;
       return NextResponse.redirect(new URL("/api/preview/enter", request.url));
     }
     previousValue = "admin";
     initializedPage.client = false;
+    isAdmin = false;
+
     return NextResponse.next();
   }
 
-  if (url === "/") {
+  if (!isAdmin) {
     if (!initializedPage.client && previousValue === "client") {
       initializedPage.client = true;
       return NextResponse.redirect(new URL("/api/preview/exit", request.url));
