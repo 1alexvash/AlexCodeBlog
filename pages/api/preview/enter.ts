16a5c8f4e1bc68cfd0271eb126b593370d5570eb
ntorbinskiy <n.torbinskiy@gmail.com>
2023-06-01 16:49:58 +0300

    Revert " Update  route"
    This reverts commit eed395c55c29e22c1995c42b669f01589273125c.

--- pages/api/preview/enter.ts
+++ pages/api/preview/enter.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   res.setDraftMode({ enable: true });
+
   res.redirect(307, "/admin/index.html");
 };
 
