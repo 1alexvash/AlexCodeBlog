690aea831e02ff2945e94fcdf9467b94afd7d656
ntorbinskiy <n.torbinskiy@gmail.com>
2023-06-20 01:04:01 +0300

    Add another test solution

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,11 @@
 /* eslint-disable @next/next/no-sync-scripts */
 import { Head, Html, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 const Document = () => (
   <Html lang="en">
     <Head>
-      <script src="/scripts/theme.js" />
+      <Script src="/scripts/theme.js" strategy="beforeInteractive" />
     </Head>
     <body>
       <Main />
