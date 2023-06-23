d27170c94dc54152658081d43ca1c9e9f0d57c2d
ntorbinskiy <n.torbinskiy@gmail.com>
2023-06-23 13:26:20 +0300

    Remove midlleware content [testing]

--- middleware.ts
+++ middleware.ts
@@ -2,16 +2,16 @@ import { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const url = request.nextUrl.pathname;
+  //   const url = request.nextUrl.pathname;
 
-  const isDraftMode = request.cookies.has("__prerender_bypass");
-  const isAdminRoot = url === "/admin";
+  //   const isDraftMode = request.cookies.has("__prerender_bypass");
+  //   const isAdminRoot = url === "/admin";
   //   const isRoot =
   //     url === "/" && request.headers.get("Sec-Fetch-Dest") !== "iframe";
 
-  if (isAdminRoot && !isDraftMode) {
-    return NextResponse.redirect(new URL("/api/preview/enter", request.url));
-  }
+  //   if (isAdminRoot && !isDraftMode) {
+  //     return NextResponse.redirect(new URL("/api/preview/enter", request.url));
+  //   }
 
   //   if (isRoot && isDraftMode) {
   //     return NextResponse.redirect(new URL("/api/preview/exit", request.url));
