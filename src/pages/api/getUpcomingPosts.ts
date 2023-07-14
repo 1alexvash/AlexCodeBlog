ae0d6577f82b852e831c91cf26b72bfaad523466
NIKITA TORBINSKIY <n.torbinskiy@gmail.com>
2023-07-14 22:54:16 +0300

    Sort posts by their date

--- src/pages/api/getUpcomingPosts.ts
+++ src/pages/api/getUpcomingPosts.ts
@@ -1,24 +1,40 @@
 import { convertTypesAndGetEdges } from "helpers/getEdgeNodesHelpers";
 import type { NextApiRequest, NextApiResponse } from "next";
 
+import { PostDocumentWithoutBody } from "../../../interfaces";
 import client from ".tina/__generated__/client";
+
+const sortFuturePosts = (
+  futurePosts: PostDocumentWithoutBody[]
+): PostDocumentWithoutBody[] => {
+  return futurePosts.sort((a, b) => {
+    if (a.date < b.date) {
+      return -1;
+    }
+    if (a.date > b.date) {
+      return 1;
+    }
+    return 0;
+  });
+};
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse
+  res: NextApiResponse<PostDocumentWithoutBody[]>
 ) {
   const draftPosts = await client.queries.postsWithoutBody({
     filter: {
       draft: { eq: true },
     },
   });
+
   const futurePosts = await client.queries.postsWithoutBody({
     filter: {
       date: { after: new Date(Date.now()).toString() },
     },
   });
 
-  const posts = convertTypesAndGetEdges(futurePosts).concat(
+  const posts = sortFuturePosts(convertTypesAndGetEdges(futurePosts)).concat(
     convertTypesAndGetEdges(draftPosts)
   );
 
