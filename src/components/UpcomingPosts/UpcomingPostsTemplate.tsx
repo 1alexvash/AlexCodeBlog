b7283e387cbcdd350a70a9b57ceee98e9a381b79
Alex <46556479+1alexvash@users.noreply.github.com>
2025-06-08 15:22:03 +0300

    Fix icon size setup

--- src/components/UpcomingPosts/UpcomingPostsTemplate.tsx
+++ src/components/UpcomingPosts/UpcomingPostsTemplate.tsx
@@ -18,8 +18,8 @@ const icons = {
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 512 512"
       fill="#000000"
-      width="24"
-      height="24"
+      width="48"
+      height="48"
     >
       <path d="M464 256A208 208 0 1 1 48 256a208 208 0 1 1 416 0zM0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM232 120l0 136c0 8 4 15.5 10.7 20l96 64c11 7.4 25.9 4.4 33.3-6.7s4.4-25.9-6.7-33.3L280 243.2 280 120c0-13.3-10.7-24-24-24s-24 10.7-24 24z" />
     </svg>
@@ -29,8 +29,8 @@ const icons = {
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 512 512"
       fill="#FFFFFF"
-      width="24"
-      height="24"
+      width="48"
+      height="48"
     >
       <path d="M464 256A208 208 0 1 1 48 256a208 208 0 1 1 416 0zM0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM232 120l0 136c0 8 4 15.5 10.7 20l96 64c11 7.4 25.9 4.4 33.3-6.7s4.4-25.9-6.7-33.3L280 243.2 280 120c0-13.3-10.7-24-24-24s-24 10.7-24 24z" />
     </svg>
@@ -40,8 +40,8 @@ const icons = {
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 448 512"
       fill="#000000"
-      width="24"
-      height="24"
+      width="48"
+      height="48"
     >
       <path d="M64 32C28.7 32 0 60.7 0 96L0 416c0 35.3 28.7 64 64 64l224 0 0-112c0-26.5 21.5-48 48-48l112 0 0-224c0-35.3-28.7-64-64-64L64 32zM448 352l-45.3 0L336 352c-8.8 0-16 7.2-16 16l0 66.7 0 45.3 32-32 64-64 32-32z" />
     </svg>
@@ -51,8 +51,8 @@ const icons = {
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 448 512"
       fill="#FFFFFF"
-      width="24"
-      height="24"
+      width="48"
+      height="48"
     >
       <path d="M64 32C28.7 32 0 60.7 0 96L0 416c0 35.3 28.7 64 64 64l224 0 0-112c0-26.5 21.5-48 48-48l112 0 0-224c0-35.3-28.7-64-64-64L64 32zM448 352l-45.3 0L336 352c-8.8 0-16 7.2-16 16l0 66.7 0 45.3 32-32 64-64 32-32z" />
     </svg>
@@ -98,7 +98,7 @@ const UpcomingPostsTemplate = ({
           },
         })}
       >
-        <Box sx={{ width: 48, height: 48 }}>{<IconComponent />}</Box>
+        <IconComponent />
         {sectionName}
       </Typography>
       <Posts posts={posts} skeletonsToRender={skeletonsCount} />
